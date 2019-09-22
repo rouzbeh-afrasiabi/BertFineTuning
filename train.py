514f37833d814f6171789e5aa2026f440095c914
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-09-22 14:12:10 -0400

    Added Req and changed train

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from BertFineTuning.data_utils import *
-from BertFineTuning.BertFineTuning import *
+from BertFineTuning.model import *
 from pytorch_transformers.optimization import AdamW
 
 
@@ -12,8 +12,9 @@ def train():
     BFT.scheduler=torch.optim.lr_scheduler.MultiStepLR(BFT.optimizer, milestones=[])
 
     ml=MultiLoader()
-
-    BFT.train(model_config,ml.bert_train_split,ml.bert_valid_split,epochs=100,print_every=100,validate_at_epoch=0)
+    
     print("Training in progress ...")
+    BFT.train(model_config,ml.bert_train_split,ml.bert_valid_split,epochs=100,print_every=100,validate_at_epoch=0)
+    
 if (__name__ == "__main__"):
     train()
